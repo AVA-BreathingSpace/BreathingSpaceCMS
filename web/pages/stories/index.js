af833fa174074ff2625f63525eb9e8b1f56946a5
lucymk <lucy.m.king92@gmail.com>
2020-06-01 13:05:52 +0100

    last test

--- web/pages/stories/index.js
+++ web/pages/stories/index.js
@@ -5,7 +5,7 @@ const StoriesStyled = styled.section.attrs({
 })``
 
 const Stories = () => {
-  return <StoriesStyled>MORE STORIES</StoriesStyled>
+  return <StoriesStyled>the last STORIES</StoriesStyled>
 }
 
 export default Stories
